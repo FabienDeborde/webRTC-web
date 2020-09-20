48be062bb1e1c6bdd7a66bb995de85769ef7778d
Fabien <fabien.webdev@gmail.com>
2020-09-20 19:25:07 +0900

    fix eslint to handle jest & fix home first test

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { screen } from '@testing-library/react'
-import { render } from './test-utils'
-import { Home } from './Home'
+import { render } from '../../test-utils'
+import Home from './Home'
 
 test('renders home page', () => {
   render(<Home />)

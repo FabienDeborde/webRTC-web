2d480a6b14b9f52b508c010514758c9030ae0931
Fabien <fabien.webdev@gmail.com>
2020-09-21 19:02:42 +0900

    use hook and refactor user video

--- src/pages/Room/UserVideo.tsx
+++ src/pages/Room/UserVideo.tsx
@@ -1,20 +1,53 @@
-import React from 'react'
-import { AspectRatioBox, Box } from '@chakra-ui/core'
+import React, { useState, useRef, useEffect } from 'react'
+import { Box } from '@chakra-ui/core'
+
+import { useUserMedia } from '../../hooks/useUserMedia'
+import { CAPTURE_OPTIONS } from '../../constants'
 
 const UserVideo: React.FC = () => {
+  const userVideoRef = useRef<HTMLVideoElement>(null)
+  const userStream = useUserMedia(CAPTURE_OPTIONS)
+  const [videoReady, setVideoReady] = useState(false)
+
+  useEffect(() => {
+    const video = userVideoRef && userVideoRef.current
+
+    if (video && userStream) {
+      video.srcObject = userStream
+      video.muted = true
+    }
+    return () => {
+      if (userStream) {
+        userStream.getTracks().forEach(track => {
+          track.stop()
+        })
+      }
+      if (video) {
+        video.pause()
+        video.srcObject = null
+        setVideoReady(false)
+      }
+    }
+  }, [userStream])
+
+  useEffect(() => {
+    const video = userVideoRef && userVideoRef.current
+    if (video && videoReady) {
+      video.play()
+    }
+  }, [videoReady])
+
   return (
-    <AspectRatioBox ratio={1}>
-      <Box
-        as="iframe"
-        title="naruto"
-        {
-          ...{
-            allowFullScreen: true,
-            src: 'https://www.youtube.com/embed/QhBnZ6NPOY0'
-          }
-        }
-      />
-    </AspectRatioBox>
+    <Box
+      as="video"
+      w='auto'
+      h='100%'
+      m={0}
+      p={0}
+      rounded="md"
+      ref={userVideoRef}
+      onCanPlay={() => setVideoReady(true)}
+    ></Box>
   )
 }
 

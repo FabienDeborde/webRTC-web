44d2b26d85ddf0185880048918bff809ed914f2f
Fabien <fabien.webdev@gmail.com>
2020-09-21 17:30:06 +0900

    update user video container

---
+++ src/pages/Room/UserVideoContainer.tsx
@@ -0,0 +1,103 @@
+import React, { useRef, useState } from 'react'
+import { Box, Button, theme } from '@chakra-ui/core'
+import UserVideo from './UserVideo'
+
+const UserVideoContainer: React.FC = () => {
+  // Manage drag & drop
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [isDragging, setIsDragging] = useState(false)
+  const [posX, setPosX] = useState(localStorage.getItem('userVideoX') ? Number(localStorage.getItem('userVideoX')) : 8)
+  const [posY, setPosY] = useState(localStorage.getItem('userVideoY') ? Number(localStorage.getItem('userVideoY')) : 8)
+  const [originalX, setOriginalX] = useState(0)
+  const [originalY, setOriginalY] = useState(0)
+  const [translateX, setTranslateX] = useState(0)
+  const [translateY, setTranslateY] = useState(0)
+
+  const handleMouseMove = (event: React.MouseEvent) => {
+    const { clientX, clientY } = event
+
+    if (isDragging) {
+      setTranslateX(clientX - originalX)
+      setTranslateY(clientY - originalY)
+    }
+  }
+
+  const handleMouseUp = () => {
+    setOriginalX(0)
+    setOriginalY(0)
+    setTranslateX(0)
+    setTranslateY(0)
+    setIsDragging(false)
+
+    const bottom = posY + (translateY * -1)
+    const right = posX + (translateX * -1)
+    setPosX(right)
+    setPosY(bottom)
+
+    localStorage.setItem('userVideoX', String(right))
+    localStorage.setItem('userVideoY', String(bottom))
+  }
+
+  const handleMouseDown = (event: React.MouseEvent) => {
+    const { clientX, clientY } = event
+
+    setOriginalX(clientX)
+    setOriginalY(clientY)
+    setTranslateX(0)
+    setTranslateY(0)
+    setIsDragging(true)
+  }
+
+  const _resetContainer = () => {
+    setPosX(8)
+    setPosY(8)
+    setOriginalX(0)
+    setOriginalY(0)
+    setTranslateX(0)
+    setTranslateY(0)
+    setIsDragging(false)
+  }
+
+  return (
+    <>
+      <Box
+        ref={containerRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        bottom={posY + 'px'}
+        right={posX + 'px'}
+        transform={`translate(${translateX}px, ${translateY}px)`}
+        w={[
+          '150px',
+          '200px'
+        ]}
+        h={[
+          '150px',
+          '200px'
+        ]}
+        pos="fixed"
+        borderWidth="1px"
+        rounded="md"
+        bg="white"
+        cursor={isDragging ? 'grabbing' : 'grab'}
+        boxShadow={isDragging ? 'lg' : 'none'}
+        zIndex={theme.zIndices.overlay}
+      >
+        <UserVideo />
+      </Box>
+      <Button
+        pos="fixed"
+        variant="outline"
+        size="xs"
+        variantColor="primary"
+        onClick={_resetContainer}
+        bottom={3}
+        right={3}
+        zIndex={theme.zIndices.docked}
+      >Reset the video</Button>
+    </>
+  )
+}
+
+export default UserVideoContainer

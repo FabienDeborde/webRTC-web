dcb111667bc16986da06f1a71eb724cb3ea60c67
Fabien <fabien.webdev@gmail.com>
2020-09-21 18:36:04 +0900

    add header in separate component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,35 +1,19 @@
 import React from 'react'
 import { Router } from '@reach/router'
 import {
-  theme,
-  Flex,
   Box
 } from '@chakra-ui/core'
 
+import { HEADER_HEIGHT } from './constants'
+import Header from './components/Header'
 import Home from './pages/Home'
 import Room from './pages/Room'
 import NotFound from './pages/NotFound'
-import { ColorModeSwitcher } from './components/ColorModeSwitcher'
-import { HEADER_HEIGHT } from './constants'
 
 const Routes: React.FunctionComponent = () => {
   return (
     <>
-      <Flex
-        as="header"
-        pos="fixed"
-        top="0"
-        left="0"
-        right="0"
-        zIndex={theme.zIndices.sticky}
-        justify="space-between"
-        align="center"
-        p="2"
-        h={`${HEADER_HEIGHT}px`}
-      >
-        <span>WebRTC tryouts</span>
-        <ColorModeSwitcher />
-      </Flex>
+      <Header />
       <Box as="main" pt={`${HEADER_HEIGHT}px`} h="100vh">
         <Router>
           <Home path="/" />

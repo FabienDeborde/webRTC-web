9bd003445ce4cb0234e6ecb6e2a6f52ea3686aaf
Fabien <fabien.webdev@gmail.com>
2020-09-24 21:47:58 +0900

    update room join (add animation, error handling, data posting)

--- src/pages/Home/HomeJoin.tsx
+++ src/pages/Home/HomeJoin.tsx
@@ -1,69 +1,120 @@
 
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { motion } from 'framer-motion'
 import {
   Box,
   Button,
   FormControl,
+  FormErrorMessage,
   FormHelperText,
   FormLabel,
-  Input
+  Input,
+  Text
 } from '@chakra-ui/core'
+import {
+  useMutation
+} from 'react-query'
+import { useNavigate } from '@reach/router'
 
-const HomeJoin = memo(function HomeCreate () {
-  const { handleSubmit, register, formState } = useForm({
+import { RoomAccessResponse, RoomAccess } from '../../typings'
+import { accessRoom } from '../../services/room'
+
+const MotionButton = motion.custom(Button)
+const MOTION_AMPLITUDE = 24
+
+interface IHomeJoin {
+  defaultID?: string;
+}
+
+const HomeJoin: React.FC<IHomeJoin> = ({ defaultID }) => {
+  const navigate = useNavigate()
+  const [formError, setFormError] = useState('')
+
+  const { handleSubmit, register, errors } = useForm({
     mode: 'onBlur',
     defaultValues: {
-      roomName: '',
-      roomPassword: ''
+      id: defaultID || '',
+      password: ''
     }
   })
 
-  function onSubmit (values: any) {
-    setTimeout(() => {
-      console.log(JSON.stringify(values, null, 2))
-    }, 1000)
+  const [joinRoom, { isLoading }] = useMutation(accessRoom, {
+    onSuccess: (res) => {
+      const data = res.data as RoomAccessResponse
+      setFormError('')
+      if (data.access) {
+        navigate(`/room/${data.id}`, {
+          state: {
+            access: true,
+            name: data.name || data.id
+          }
+        })
+      } else {
+        setFormError('Sorry but you do not have access to this room.\n Either the room does not exists anymore, or your password is not correct.')
+      }
+    },
+    onError: (error: Error) => {
+      setFormError(error && error.message)
+    }
+  })
+
+  const onSubmit = async (data: RoomAccess) => {
+    if (!isLoading) {
+      joinRoom(data)
+    }
   }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl>
-        <FormLabel htmlFor="roomName">Room name</FormLabel>
+        <FormLabel htmlFor="id">Room ID</FormLabel>
         <Input
-          name="roomName"
+          name="id"
           placeholder="example: 3sa54c6rdtv7yfui"
           size="lg"
           variant="flushed"
           ref={register}
+          data-lpignore="true"
+          autoFocus
         />
+        <FormErrorMessage>
+          {errors.id && errors.id.message}
+        </FormErrorMessage>
       </FormControl>
       <FormControl mt={6}>
-        <FormLabel htmlFor="roomName">Room password</FormLabel>
+        <FormLabel htmlFor="password">Room password</FormLabel>
         <Input
-          name="roomPassword"
+          name="password"
           placeholder=""
           size="lg"
           variant="flushed"
           ref={register}
           type="password"
+          data-lpignore="true"
         />
+        <FormErrorMessage>
+          {errors.password && errors.password.message}
+        </FormErrorMessage>
         <FormHelperText>
           Enter a password here if the room is protected.
         </FormHelperText>
       </FormControl>
       <Box>
-        <Button
+        <MotionButton
           mt={6}
           variantColor="primary"
-          isLoading={formState.isSubmitting}
+          isLoading={isLoading}
           type="submit"
           w="100%"
+          animate={formError ? { x: [-MOTION_AMPLITUDE, MOTION_AMPLITUDE, -MOTION_AMPLITUDE, MOTION_AMPLITUDE, -MOTION_AMPLITUDE, MOTION_AMPLITUDE, 0] } : undefined}
         >
         Join
-        </Button>
+        </MotionButton>
       </Box>
+      <Text mt={2} color="red.300">{formError}</Text>
     </form>
   )
-})
+}
 
-export default HomeJoin
+export default memo(HomeJoin)
